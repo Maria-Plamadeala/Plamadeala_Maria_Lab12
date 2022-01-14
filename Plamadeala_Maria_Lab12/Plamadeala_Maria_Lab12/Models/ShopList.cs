06470d043191f973d3b3c3030d0072ebd0f44b90
maria_plamadeala@yahoo.com <maria_plamadeala@yahoo.com>
2022-01-14 20:06:01 +0200

    Add project files.

---
+++ Plamadeala_Maria_Lab12/Plamadeala_Maria_Lab12/Models/ShopList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plamadeala_Maria_Lab12.Models
+{
+    public class ShopList
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
